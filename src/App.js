4222d170e90626965c17519e80fdbe5f97786fab
Semih Alper Keskin <semihalperkeskin@gmail.com>
2022-07-19 23:39:09 +0300

    use router

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
+import { BrowserRouter, Route, Routes, Link } from 'react-router-dom';
+import HomePage from "../src/Pages/HomePages"
+import About from "../src/Pages/About"
+import Contact from "../src/Pages/Contact"
 import './App.css';
+import NoPage from './Pages/NoPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+      <BrowserRouter>
+      <nav className="navbar navbar-expand-lg">
+      <div className="container-fluid">
+        <div className="container collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <Link className='nav-link' to="/" >Home</Link>
+            </li>
+            <li className="nav-item">
+              <Link className='nav-link' to="/About" >About</Link>
+            </li>
+            <li className="nav-item">
+              <Link className='nav-link' to="/Contact">Contact</Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+        <Routes>
+          {/* <Route path='/' element={<Layout />}/> */}
+            <Route index element={<HomePage />} />
+            <Route path="/About" element={<About />} />
+            <Route path="/Contact" element={<Contact />} />
+            <Route path='*' element={<NoPage />} />
+        </Routes>
+      </BrowserRouter>
+    
   );
+}
+
+const Layout = () => {
+  return (
+    <h1>agnjfdnbjfdnbjdffbnjfdjnbdf</h1>
+  )
 }
 
 export default App;
---
+++ src/Pages/HomePages.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function HomePages() {
+  return (
+    <div className="container">
+    <h1>HomePages</h1>
+    </div>
+  )
+}
